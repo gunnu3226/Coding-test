fc4d5b15cf86519366ec444c45d9a463a8409dab
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-20 11:26:08 +0900

    Update 동전 0.py

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -11,4 +11,19 @@ for coin in coin_list:
         break
 print(n)
 
-        
+#함수형태의 코드
+n, k = map(int,input().split())
+coin_list = []
+for i in range(n):
+    coin_list.append(int(input()))
+coin_list.reverse()
+
+def coin_min(k, coin_list):
+    n = 0
+    for coin in coin_list:
+        n += k // coin
+        k %= coin
+        if k == 0:
+            break
+    return n
+print(coin_min(k,coin_list))
