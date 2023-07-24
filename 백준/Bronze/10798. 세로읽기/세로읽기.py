ba5a0bd7f7fa8f80cd1ae4f743e27a87b8d57b72
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-24 23:53:53 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,12 @@
+words = []
+length = []
+ans = ''
+for _ in range(5):
+    word = input()
+    words.append(word)
+    length.append(len(word))
+for i in range(max(length)):
+    for j in range(5):
+        if i < length[j]:
+            ans += words[j][i]
+print(ans)
