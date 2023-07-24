2956e1465d8711d36ae6e4515731302e40d148e5
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-24 22:53:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,76 +1,19 @@
 def solution(survey, choices):
     answer = ''
-    R = 0
-    T = 0
-    C = 0
-    F = 0
-    J = 0
-    M = 0
-    A = 0
-    N = 0
-    grade = {1:3,
-            2:2,
-            3:1,
-            4:0,
-            5:1,
-            6:2,
-            7:3}
-    for i in range(len(survey)):
-        if survey[i] == 'AN':
-            if choices[i] <= 3:
-                A += grade[choices[i]]
-            elif choices[i] >=5:
-                N += grade[choices[i]]
-        elif survey[i] == 'NA':
-            if choices[i] <= 3:
-                N += grade[choices[i]]
-            elif choices[i] >=5:
-                A += grade[choices[i]]
-        elif survey[i] == 'CF':
-            if choices[i] <= 3:
-                C += grade[choices[i]]
-            elif choices[i] >=5:
-                F += grade[choices[i]]
-        elif survey[i] == 'FC':
-            if choices[i] <= 3:
-                F += grade[choices[i]]
-            elif choices[i] >=5:
-                C += grade[choices[i]]
-        elif survey[i] == 'JM':
-            if choices[i] <= 3:
-                J += grade[choices[i]]
-            elif choices[i] >=5:
-                M += grade[choices[i]]
-        elif survey[i] == 'MJ':
-            if choices[i] <= 3:
-                M += grade[choices[i]]
-            elif choices[i] >=5:
-                J += grade[choices[i]]
-        elif survey[i] == 'RT':
-            if choices[i] <= 3:
-                R += grade[choices[i]]
-            elif choices[i] >=5:
-                T += grade[choices[i]]
-        elif survey[i] == 'TR':
-            if choices[i] <= 3:
-                T += grade[choices[i]]
-            elif choices[i] >=5:
-                R += grade[choices[i]]
-    if R >= T:
-        answer += 'R'
-    else:
-        answer += 'T'
-    if C >= F:
-        answer += 'C'
-    else:
-        answer += 'F'
-    if J >= M:
-        answer += 'J'
-    else:
-        answer += 'M'
-    if A >= N:
-        answer += 'A'
-    else:
-        answer += 'N'
-        
+    dic = {'RT':0,'CF':0,'JM':0,'AN':0}
+    for A,B in zip(survey,choices):
+        if A not in dic.keys():
+            A = A[::-1]
+            dic[A] -= B-4
+        else: 
+            dic[A] += B-4
+            
+    for name in dic.keys():
+        if dic[name]>0:
+            answer += name[1]
+        elif dic[name]<0:
+            answer += name[0]
+        else:
+            answer += sorted(name)[0]
+            
     return answer
