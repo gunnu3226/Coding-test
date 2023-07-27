2451f2d9c8fd77f689c4fa507d882bbb3a43c474
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-27 15:20:00 +0900

    Update 신규 아이디 추천.py

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -17,3 +17,24 @@ def solution(new_id):
     while len(new) <=2:
         new += new[-1]
     return new
+
+
+import re
+
+def solution(new_id):
+    new = new_id
+    #1단계
+    new = new.lower()
+    #2단계
+    new = re.sub('[^a-z0-9-_.]','',new)
+    #3단계
+    new = re.sub('\.+','.',new)
+    #4단계
+    new = re.sub('^[.]|[.]$','',new)
+    #5단계,6단계
+    new = 'a' if len(new)==0 else new[:15]
+    new = re.sub('^[.]|[.]$','',new)
+    #7단계
+    while len(new) <=2:
+        new += new[-1]
+    return new
