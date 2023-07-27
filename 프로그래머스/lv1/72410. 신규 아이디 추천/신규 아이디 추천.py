b44bd791c821e8360dec19ac3e1f399775ea1902
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-27 15:19:39 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,31 +1,19 @@
 import re
 
 def solution(new_id):
-    new_id = new_id.lower()  #1단계
-    
-    new_id = re.sub(r"[^a-z0-9-_.]", "", new_id) #2단계
-    
-    new_id = re.sub(r"\.{2,}", ".", new_id) # 3단계
-    
-    while new_id[0] == '.' or new_id[-1] == '.':#4단계
-        if len(new_id) == 1:
-            new_id = ''
-            break
-        if new_id[0] == '.': 
-            new_id = new_id[1:]
-        if new_id[-1] == '.':
-            new_id = new_id[:-1]
-        
-    
-    if len(new_id) == 0: #5단계
-        new_id += 'a'
-    
-    if len(new_id)>=16: #6단계
-        new_id = new_id[:15]
-        while new_id[-1] == '.':
-            new_id = new_id[:-1]
-            
-    if len(new_id)<=2:
-        while len(new_id)<3:
-            new_id += new_id[-1]
-    return new_id
+    new = new_id
+    #1단계
+    new = new.lower()
+    #2단계
+    new = re.sub('[^a-z0-9-_.]','',new)
+    #3단계
+    new = re.sub('\.+','.',new)
+    #4단계
+    new = re.sub('^[.]|[.]$','',new)
+    #5단계,6단계
+    new = 'a' if len(new)==0 else new[:15]
+    new = re.sub('^[.]|[.]$','',new)
+    #7단계
+    while len(new) <=2:
+        new += new[-1]
+    return new
