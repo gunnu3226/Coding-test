94336d0deab74b9157d8c88ab6bb87a3f7cffe77
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-07-27 09:43:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,14 +1,21 @@
-word = input()
-n = int(input())
+import sys
+input = sys.stdin.readline
+s = list(input().rstrip())
+n = int(input().rstrip())
+
+al_list = [[0]*len(s) for _ in range(26)]
+for i in range(len(s)):
+    al_list[ord(s[i])-ord('a')][i] += 1
+
+for i in range(26):
+    for j in range(1,len(s)):
+        al_list[i][j] += al_list[i][j-1]
+        
 for i in range(n):
-    al, l, r = input().split()
-    l = int(l)
-    r = int(r)
-    ans = 0
-    for j in range(l,r+1):
-        if word[j]==al:
-            ans += 1
-    print(ans)
-     
-            
-            
+    a, l, r = input().split()
+    tmp = al_list[ord(a)-ord('a')][int(r)]-al_list[ord(a)-ord('a')][int(l)]
+    if s[int(l)] == a:
+           tmp += 1
+    print(tmp)
+           
+    
