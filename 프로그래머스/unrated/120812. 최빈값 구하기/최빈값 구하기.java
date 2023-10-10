f67a798d8b8bee9ed613030e2797af5eab38dd7a
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-10-11 00:12:12 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int frequent[] = new int[1000];
+        int max=0;
+        
+        for(int i=0;i<array.length;i++){
+            frequent[array[i]]++;
+            
+            if(max<frequent[array[i]]){
+                max = frequent[array[i]];
+                answer = array[i];
+            }
+        }
+        
+        int temp = 0;
+        for(int j=0;j<1000;j++){
+            if(max==frequent[j]){
+                temp++;
+            }
+            if(temp>1){
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
