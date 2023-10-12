55e9de216cdb508d350732be1a26f51d138731f7
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-10-12 23:05:56 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
-        String[] strarr = str1.split(str2);
-        if(str1.contains(str2)){
-            answer = 1;
-        }else{
-            answer = 2;
-        }
-        return answer;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
     
