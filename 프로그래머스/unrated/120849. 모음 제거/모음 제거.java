ead112313deb5b5d734976b005f557cafce6da3b
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-10-13 23:51:37 +0900

    [level unrated] Title: 모음 제거, Time: 0.05 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String[] vowels = {"a","e","i","o","u"};
+        
+        for(String v : vowels){
+            my_string = my_string.replace(v,"");
+        }
+        return my_string;
+    }
+}
