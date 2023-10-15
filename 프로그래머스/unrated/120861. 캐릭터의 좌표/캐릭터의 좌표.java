5a2e25899b79d1598a5efffe402f4d5d1bcd4281
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-10-15 19:09:46 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,19 +1,58 @@
 class Solution {
-    public int[] solution(String[] keyinput, int[] board) {
+    String up = "up";
+        String down = "down";
+        String left = "left";
+        String right = "right";
         int[] answer = {0,0};
-        int row_max = board[0]/2;
-        int col_max = board[1]/2;
+        int max_heigt = 0;
+        int max_width = 0;
+    
+    public int[] solution(String[] keyinput, int[] board) {
+        max_heigt = board[1]/2;
+        max_width = board[0]/2;
         for(String key : keyinput){
-            if(key.equals("left")&&answer[0]>-row_max){
-                answer[0]--;
-            }else if(key.equals("right")&&answer[0]<row_max){
-                answer[0]++;
-            }else if(key.equals("up")&&answer[1]<col_max){
-                answer[1]++;
-            }else if(key.equals("down")&&answer[1]>-col_max){
-                answer[1]--;
-            }
-        }     
+            move(key);
+        }
         return answer;
+    }
+    
+    public void move(String key){
+        if(up.equals(key)){
+            moveUp();
+        }else if(down.equals(key)){
+            moveDown();
+        }else if(left.equals(key)){
+            moveLeft();
+        }else if(right.equals(key)){
+            moveRight();
+        }
+    }
+    
+    public void moveUp(){
+        int top = max_heigt;
+        if(answer[1]<top){
+            answer[1]++;
+        };
+    }
+    
+    public void moveDown(){
+        int bottom = max_heigt*-1;
+        if(answer[1]>bottom){
+            answer[1]--;
+        };
+    }
+    
+    public void moveRight(){
+        int right = max_width;
+        if(answer[0]<right){
+            answer[0]++;
+        };
+    }
+    
+    public void moveLeft(){
+        int left = max_width*-1;
+        if(answer[0]>left){
+            answer[0]--;
+        };
     }
 }
