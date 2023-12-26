477aeb59e472b550ca20ea1c4d57c753e5abd964
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-12-26 17:30:09 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.05 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,26 +1,28 @@
+import java.util.*;
+
 class Solution {
     public String solution(String X, String Y) {
         StringBuilder answer = new StringBuilder();
-        int[] x = {0,0,0,0,0,0,0,0,0,0};
-        int[] y = {0,0,0,0,0,0,0,0,0,0};
-        for(int i=0; i<X.length();i++){
-            x[X.charAt(i)-48] += 1;
-        }
-        for(int i=0; i<Y.length();i++){
-            y[Y.charAt(i)-48] += 1;
-        }
+        int[] arrx = new int[10];
+        int[] arry = new int[10];
 
-        for(int i=9; i >= 0; i--){
-            for(int j=0; j<Math.min(x[i],y[i]); j++){
+        for (int i = 0; i < X.length(); i++) {
+            arrx[X.charAt(i)-48]++;
+        }
+        for (int i = 0; i < Y.length(); i++) {
+            arry[Y.charAt(i)-48]++;
+        }
+        for (int i = 9; i >= 0; i--) {
+            int min = Math.min(arrx[i], arry[i]);
+            for (int j = 0; j < min; j++) {
                 answer.append(i);
             }
         }
-        if("".equals(answer.toString())){
+        if ("".equals(answer.toString())) {
             return "-1";
-        }else if(answer.toString().charAt(0)==48){
+        } else if (answer.toString().charAt(0)==48) {
             return "0";
-        }else {
-            return answer.toString();
         }
+        return answer.toString();
     }
 }
