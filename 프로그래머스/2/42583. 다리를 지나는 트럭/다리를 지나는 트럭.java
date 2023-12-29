733ee0ba6dec445e51c2723b60ad40c6eb995219
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-12-29 20:50:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.76 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -4,32 +4,52 @@ import java.util.Queue;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Queue<Integer> bridge = new LinkedList<>();
-        int sum = 0;
+        Queue<Truck> wait = new LinkedList<>();
+        Queue<Truck> bridge = new LinkedList<>();
 
         for(int truck : truck_weights) {
+            wait.offer(new Truck(truck));
+        }
+        int total = 0;
+
+        while(!wait.isEmpty() || !bridge.isEmpty()) {
+            answer++;
+
+            if(bridge.isEmpty()) {
+                Truck truck = wait.poll();
+                total += truck.weight;
+                bridge.offer(truck);
+                continue;
+            }
+
+            for(Truck t : bridge) {
+                t.moving();
+            }
+
+            if(bridge.peek().move > bridge_length) {
+                total -= bridge.poll().weight;
+            }
             
-            while(true) {
-                if(bridge.isEmpty()) {
-                    bridge.offer(truck);
-                    sum += truck;
-                    answer++;
-                    break;
-                } else if (bridge.size() == bridge_length) {
-                    sum -= bridge.poll();
-                } else {
-                    if(sum + truck > weight) {
-                        bridge.offer(0);
-                        answer++;
-                    } else {
-                        bridge.offer(truck);
-                        sum += truck;
-                        answer++;
-                        break;
-                    }
-                }
+            if(!wait.isEmpty() && total + wait.peek().weight <= weight) {
+                Truck t = wait.poll();
+                total += t.weight;
+                bridge.offer(t);
             }
         }
-        return answer + bridge_length;
+        return answer;
+    }
+}
+
+class Truck {
+    int weight;
+    int move;
+
+    public Truck(int weight) {
+        this.weight = weight;
+        this.move = 1;
+    }
+
+    public void moving() {
+        move++;
     }
 }
