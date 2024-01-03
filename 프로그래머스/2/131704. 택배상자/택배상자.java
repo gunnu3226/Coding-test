f5107af0f9d0343cac4d22c23a9f34f721d51932
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-01-03 12:50:06 +0900

    [level 2] Title: 택배상자, Time: 64.17 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,35 +1,23 @@
 import java.util.Stack;
 
 class Solution {
-
-    public static void main(String[] args) {
-        solution(new int[]{4, 3, 1, 2, 5});
-    }
     public static int solution(int[] orders) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<Integer>();
-        int cnt = 1;
-        for (int order : orders) {
-                while(cnt <= orders.length) {
-                    if (cnt == order) {
-                        break;
-                    } else if(!stack.isEmpty() && stack.peek() == order) {
-                        break;
-                    }else {
-                        stack.push(cnt);
-                        cnt++;
-                    }
-                }
-                if(cnt == order) {
-                    cnt++;
-                    answer++;
-                } else if(!stack.isEmpty() && stack.peek() == order) {
-                    stack.pop();
-                    answer++;
-                } else {
-                    break;
-                }
+        Stack<Integer> stack = new Stack<>();
+        int idx = 0;
+        for(int i=1; i<=orders.length; i++) {
+            if(i == orders[idx]) {
+                answer++;
+                idx++;
+            } else {
+                stack.add(i);
             }
+            while(!stack.isEmpty() && stack.peek() == orders[idx]) {
+                stack.pop();
+                answer++;
+                idx++;
+            }
+        }
         return answer;
     }
 }
