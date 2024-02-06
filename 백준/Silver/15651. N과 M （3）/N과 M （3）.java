a41fa7f73c6159e6cc509c7eb95492307e340f2d
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-02-06 10:59:01 +0900

    [Silver III] Title: N과 M (3), Time: 576 ms, Memory: 158056 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+
+    static int n,m;
+    static int[] arr;
+    static boolean[] check;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        check = new boolean[n+1];
+        dfs(0);
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int depth) throws IOException {
+        if(depth == m) {
+            for (int i : arr) {
+                bw.write(i+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i=1; i<=n; i++) {
+            arr[depth] = i;
+            dfs(depth+1);
+        }
+    }
+}
