9842363f6650f0021f7722e515138368124963aa
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-02-08 00:54:34 +0900

    [Silver II] Title: N과 M (11), Time: 1072 ms, Memory: 150784 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,m;
+    static int[] arr;
+    static int[] nums;
+    static boolean[] check;
+    static Set<String> set;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+        for(int i=0; i<n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        check = new boolean[n];
+        arr = new int[m];
+
+        set = new LinkedHashSet<>();
+
+        dfs(0);
+        for (String str : set) {
+            bw.write(str+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int depth) throws IOException {
+        if(depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            set.add(sb.toString());
+            return;
+        }
+        for (int i=0; i<n; i++) {
+//            if(check[i]) continue;
+            arr[depth] = nums[i];
+//            check[i] = true;
+            dfs(depth+1);
+//            check[i] = false;
+        }
+    }
+}
