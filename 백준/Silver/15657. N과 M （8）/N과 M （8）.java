7b08876b97c4858205336b1fa7cfb0efec3a6470
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-02-08 00:09:53 +0900

    [Silver III] Title: N과 M (8), Time: 212 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,m;
+    static int[] arr;
+    static int[] nums;
+    static boolean[] check;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+        for(int i=0; i<n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        check = new boolean[n];
+        arr = new int[m];
+        dfs(0, 0);
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int depth, int min) throws IOException {
+        if(depth == m) {
+            for (int i : arr) {
+                bw.write(i+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i=min; i<n; i++) {
+            arr[depth] = nums[i];
+            dfs(depth+1, i);
+        }
+    }
+}
