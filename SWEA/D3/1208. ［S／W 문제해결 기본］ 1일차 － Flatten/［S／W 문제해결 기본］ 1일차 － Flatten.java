c898aa5f94b2f9a692ed383f9d25b45cabbd4e96
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-05-13 20:23:40 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 165 ms, Memory: 34,104 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,33 +1,28 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+class Solution {
 
-class Solution
-{
-	public static void main(String args[]) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		for(int tc = 1; tc <= 10; tc++){
-			int n = sc.nextInt();
-			List<Integer> list = new ArrayList<>();
-			for(int i=0;i<100;i++) {
-				list.add(sc.nextInt());
-			}
-			for(int j=0;j<n;j++) {
-				
-				int maxindex = list.indexOf(Collections.max(list));
-				int minindex = list.indexOf(Collections.min(list));
-				
-				list.set(maxindex, list.get(maxindex)-1);
-				list.set(minindex, list.get(minindex)+1);
-				
-				if(Collections.max(list)-Collections.min(list)<=1) {
-					break;
-				}
-			}
-			System.out.println("#"+tc+" "+(Collections.max(list)-Collections.min(list)));
-		}
-	}
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+//        int T = sc.nextInt();
+
+        for(int tc = 1; tc <= 10; tc++){
+            int n = sc.nextInt();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<100; i++) {
+                list.add(sc.nextInt());
+            }
+            for(int j=0; j<n; j++) {
+                int maxIndex = list.indexOf(Collections.max(list));
+                int minIndex = list.indexOf(Collections.min(list));
+
+                list.set(maxIndex, list.get(maxIndex)-1);
+                list.set(minIndex, list.get(minIndex)+1);
+            }
+            System.out.println("#"+tc+" "+(Collections.max(list)-Collections.min(list)));
+        }
+    }
 }
