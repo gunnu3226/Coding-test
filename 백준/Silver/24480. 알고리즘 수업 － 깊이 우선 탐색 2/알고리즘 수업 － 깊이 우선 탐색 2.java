2a8f9b56540aa610353b1da85b7d268a55e0192c
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-07-12 22:00:01 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 900 ms, Memory: 87564 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n, m, r;
+    static int[] visited;
+    static List<Integer>[] graph;
+    static int visitOrder = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        //int[start][end]
+        visited = new int[n+1];
+        graph = new ArrayList[n+1];
+
+        for(int i=1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+
+        for(int i=1; i<=n; i++) {
+            graph[i].sort(Collections.reverseOrder());
+        }
+
+        dfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int start) {
+        visited[start] = visitOrder++;
+        for(int next : graph[start]) {
+            if (visited[next] == 0) {
+                dfs(next);
+            }
+        }
+    }
+}
