74d626b7f777c104a723decb2cfdcbd5176337a0
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2025-06-30 21:59:59 +0900

    [Bronze III] Title: ZOAC 4, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int vertical = (H + N) / (N + 1);
+        int horizontal = (W + M) / (M + 1);
+
+        System.out.println(vertical * horizontal);
+    }
+}
