3f8808239dd41c9d6aad0d236b2430fa82385ea0
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2025-09-22 20:50:43 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.25 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int friendsLength = friends.length;
+        
+        Map<String, Integer> friendsMap = new HashMap<>();
+        
+        for(int i = 0; i < friendsLength;  i++) {
+            friendsMap.put(friends[i], i);
+        }
+        
+        int[] giftDegree = new int[friendsLength];
+        int[][] giftGraph = new int[friendsLength][friendsLength];
+        
+        for(String gift : gifts) {
+            String[] giftName = gift.split(" ");
+            
+            Integer from = friendsMap.get(giftName[0]);
+            Integer to = friendsMap.get(giftName[1]);
+        
+            giftDegree[from]++;
+            giftDegree[to]--;
+            giftGraph[from][to]++;
+        }
+        
+        int answer = 0;
+        
+        for(int i = 0; i < friendsLength; i++) {
+            int count = 0;
+            
+            for(int j = 0; j < friendsLength; j++) {
+                
+                if(i == j) {
+                    continue;
+                }
+                
+                if(giftGraph[i][j] > giftGraph[j][i] || 
+                   (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
+                    count++;                    
+                }
+                   
+               if(answer < count) {
+                   answer = count;
+               }
+            
+            }
+        }
+        return answer;
+    }
+}
